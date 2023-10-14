a85f68c1c8924f2b5125c24dff2680203a7c200a
Mohammed Siddic <shawnsid10@gmail.com>
2023-10-14 14:49:18 +0530

    card-qtify-takehome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
+import { fetchTopAlbums } from './api/api';
+import Card from './components/Card/Card';
 
 function App() {
+
+  const [topAlbumsData, setTopAlbumsData] = useState([]);
+
+  const generateTopAlbumsData = async () => {
+    try{
+      const data = await fetchTopAlbums();
+      setTopAlbumsData(data)
+    }catch(e){
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    generateTopAlbumsData();
+  }, [])
+
   return (
     <div>
       <Navbar />
+      <Hero />
+      {
+        topAlbumsData.map((item) => {
+          return (
+            <Card data={item} type="album" key={item.title}/>
+          )
+        })
+      }
     </div>
   );
 }
