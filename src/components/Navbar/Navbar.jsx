74ebafe2051b5cabf02faeecc6b32f1383441245
Mohammed Siddic <shawnsid10@gmail.com>
2023-10-14 10:49:01 +0530

    search

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Button from '../Button/Button';
 import styles from './Navbar.module.css';
 import Logo from '../Logo/Logo'
+import Search from '../Search/Search';
 
 const Navbar = () => {
   return (
    <nav className={styles.navbar}>
     <Logo />
-    <Button children= "Give Feedback" />
+    <Search placeholder = 'Search album or song of your choice' />
+    <Button children = "Give Feedback" />
    </nav>
   )
 }
